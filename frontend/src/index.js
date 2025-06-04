5f3eaf494a70a61537dc01dba1b3a8dd59a77503
ToT <114044322+TangoOscarTango@users.noreply.github.com>
2025-06-04 09:28:59 -0400

    Update index.js
    tixtactoe implementation 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
+const { StrictMode } = React;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
