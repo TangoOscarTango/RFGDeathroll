e008a1834e9e76fa25665e3febe9ce8c1de1f950
ToT <114044322+TangoOscarTango@users.noreply.github.com>
2025-06-04 09:39:48 -0400

    Update index.js
    revert from ttt

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
-const { StrictMode } = React;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
