2f99c7cf64de455e816abcce0f75518e965484fd
ToT <114044322+TangoOscarTango@users.noreply.github.com>
2025-06-04 20:00:00 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ app.use(cors({
 
 app.use(express.json());
 
+console.log('MONGO_URI:', process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
