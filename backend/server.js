c60b71e74731a907b1fd3aa6f21ee513e2d66f3b
ToT <114044322+TangoOscarTango@users.noreply.github.com>
2025-06-04 20:38:33 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
